cb8e3c91ba81cd62b345c8c81ea78c3789ae0511
Nathan Huesmann <nhuesmann@gmail.com>
2022-12-11 13:52:06 -0800

    Creates functional project for: - typescript - react - tailwind - vite

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-6xl">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
